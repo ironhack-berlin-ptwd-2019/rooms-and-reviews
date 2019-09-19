ebfb205ae023a2ec0b94a32d372a2f6d1c3bf6f8
Hendrik <hendrik.wermser@ironhack.com>
2019-09-19 19:16:17 +0200

    adds board, corrects description for list model

--- models/list.js
+++ models/list.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const listSchema = new Schema({
   name: { type: String },
-  description: { type: String },
+  description: { type: String, default: '<none>' },
   imageUrl: { type: String },
-  owner: { type: Schema.Types.ObjectId, ref: 'User' }
+  owner: { type: Schema.Types.ObjectId, ref: 'User' },
+  board: { type: Schema.Types.ObjectId, ref: 'Board' }
 }, {
     timestamps: true
   });
